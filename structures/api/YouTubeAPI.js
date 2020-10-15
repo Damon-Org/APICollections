995f00fdfce0d3cd9bea322c6f8a69b53969ac36
Yimura <andreas.maerten@scarlet.be>
2020-10-15 12:55:15 +0200

    YouTubeAPI: Added try/catch in case YouTube denies a request

--- structures/api/YouTubeAPI.js
+++ structures/api/YouTubeAPI.js
@@ -1,23 +1,35 @@
 import youtube from 'scrape-youtube'
+import { YTDefaultSearchOptions } from '../../util/Constants.js'
 
 export default class YouTubeAPI{
     constructor() {
         this.yt = youtube.default;
     }
 
-    /**
-     * @param {String} searchFor A String to search for on Youtube
-     * @returns {Promise<Object>} JSON data
-     */
-    search(searchFor) {
-        return this.yt.search(searchFor, { limit: 5, type: 'video' });
+    _mergeOptions(...args) {
+        return Object.assign({}, YTDefaultSearchOptions, ...args);
     }
 
     /**
-     * @param {String} searchFor A String to search for on Youtube
+     * @param {string} searchFor A String to search for on Youtube
+     * @param {Object} [options={}] The options to pass to the search scraper
+     * @returns {Promise<Object>|Promise<null>} Returns JSON data or null incase of an error
+     */
+    async search(searchFor, options = {}) {
+        options = this._mergeOptions(options);
+
+        try {
+            return await this.yt.search(searchFor, options);
+        } catch (e) {
+            return null;
+        }
+    }
+
+    /**
+     * @param {string} searchFor A String to search for on Youtube
      * @returns {Promise<Object>} JSON data
      */
     searchOne(searchFor) {
-        return this.yt.search(searchFor, { limit: 1, type: 'video' });
+        return this.search(searchFor, { limit: 1, type: 'video' });
     }
 }

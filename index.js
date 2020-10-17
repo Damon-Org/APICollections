66e44e6037b992d7cf2138cdf717eb5da35ef750
Yimura <andreas.maerten@scarlet.be>
2020-10-17 23:07:47 +0200

    Index: Commenting SpotifyAPI as it is no longer used

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ export default class APICollections extends BaseModule {
 
     setup() {
         this.youtube = new YouTubeAPI();
-        this.spotify = new SpotifyAPI(this._m, this.auth.credentials.api.spotify);
+        //this.spotify = new SpotifyAPI(this._m, this.auth.credentials.api.spotify);
         this.deezer = new DeezerAPI(this._m);
 
         return true;
